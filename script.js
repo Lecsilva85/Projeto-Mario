ef028aeb3c7e5bbe47ac1718a4dafbbbba1f97b3
Lecsilva85 <devlecsilva@gmail.com>
2025-04-17 22:34:17 -0300

    Adicionado Responsividade

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const form = document.querySelector('.foms-fale-conosco');
-const mascara = document.querySelector('.mascara-formulario');
-const fechamento = document.querySelector('.form-fale-conosco');world
+const form = document.querySelector('.foms-fale-conosco')
+const mascara = document.querySelector('.mascara-formulario')
+const fechamento = document.querySelector('.form-fale-conosco')
 
 function botaoClicado(){
-    form.style.left = "45%"
+    form.style.left = "auto"
     form.style.transform = "translateX(0%)"
     form.style.transition = "1s"
     mascara.style.visibility = "visible"
